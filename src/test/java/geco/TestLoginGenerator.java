f08f122952e9fd17cbce2affdad2dafab3addee6
= <julien-labadens@orange.fr>
2020-11-17 09:33:06 +0100

    fix #3 bug fixé

--- src/test/java/geco/TestLoginGenerator.java
+++ src/test/java/geco/TestLoginGenerator.java
@@ -1,2 +1,24 @@
-package geco;public class TestLoginGenerator {
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestLoginGenerator {
+    LoginService loginService = new LoginService(new String[] {"JROL",
+            "BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
+
+    @Test
+    public void CT1(){
+        LoginGenerator generator = new LoginGenerator(loginService);
+        assertEquals("paul durand added",
+               generator.generateLoginForNomAndPrenom("Duran","Paul"),"PDUR");
+    }
+    @Test
+    public void CT2(){
+        LoginGenerator generator = new LoginGenerator(loginService);
+        assertEquals("John Ralling added",
+                generator.generateLoginForNomAndPrenom("Ralling","John"),"JRAL1");
+    }
+
 }
